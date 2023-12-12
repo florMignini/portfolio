7457ed21de3e73183b63dc8e0bd609f7ec822b0c
florMignini <flormignini29@gmail.com>
2023-12-12 09:01:48 -0300

    mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import logo from "../assets/florencia-favicon-color.png";
+import { BurguerIcon, CloseIcon } from "../icons";
 type link = {
   id: string;
   title: string;
@@ -12,7 +13,9 @@ const navLink = [
   { id: "3", title: "Contact" },
 ];
 export const Navbar = () => {
-  const [active, setActive] = useState("");
+  const [active, setActive] = useState<string>("");
+  // burguer menu state
+  const [toggle, setToggle] = useState<boolean>(false);
   return (
     <nav
       className={`${styles.paddingX} w-full  flex items-center py-1 fixed top-0 z-20 bg-primary`}
@@ -45,6 +48,39 @@ export const Navbar = () => {
           ))}
         </ul>
         {/* mobile nav */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <button
+            className="w-7 h-7 object-contain flex 
+            cursor-pointer mr-2"
+            onClick={() => setToggle(!toggle)}
+          >
+            {toggle ? <CloseIcon /> : <BurguerIcon />}
+          </button>
+
+          {/* content */}
+          <div
+            className={`${
+              !toggle ? `hidden` : `flex`
+            } p-6 black-gradient absolute top-10 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-lg`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLink.map((link: link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins text-sm font-medium cursor-pointer`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
---
+++ src/icons/index.ts
@@ -0,0 +1,2 @@
+export * from './BurguerIcon';
+export * from './CloseIcon';
