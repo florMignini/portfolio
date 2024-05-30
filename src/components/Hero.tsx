197b58483bdc13a8a5490c3341862928fdc93f3d
“florMignini” <flormignini29@gmail.com>
2024-05-30 09:12:44 -0300

    hero content update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,32 +1,36 @@
 import { styles } from "../styles";
-// import me from "../assets/me-hero.png";
+import me from "../assets/github.png";
 export const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto bg-blur bg-background text-black">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[150px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-48 w-[100%] mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="absolute left-28 lg:pl-0 flex flex-col justify-center items-center top-24">
-        </div>
-        {/* info section */}
-        {/* <div
-          className="h-[80%] w-fill absolute left-36 bottom-5 flex flex-col gap-5 
-text-3xl
+        {/* hero upper section */}
+        <div
+          className="h-[80%] w-[50%] absolute left-1 top-0 flex flex-col gap-5 
+text-3xl text-[#4f3cbf] text-end
 "
         >
-          <h1 className="text-5xl font-bold mt-5">
-            Hi, I'm <strong className="text-[#4f3cbf] ">Florencia.</strong>
-          </h1>
-          <h3 className="text-3xl font-bold mt-2 text-[#692cee]">
-            Software Engineer Fullstack <br className="sm:block hidden" />
-          </h3>
-            <small className="w-[70%] font-thin text-lg">
-              I love to build beautiful, intuitive and accessible digital experiences with effective usability for the
-              web.
-            </small>
-        </div> */}
+          <div className="w-[95%]">
+            <h1 className="text-5xl font-light ">
+              Hi, I'm <strong className="text-bold text-6xl">Florencia.</strong>
+            </h1>
+            <h3 className="w-[100%]  text-3xl font-bold mt-5 text-text">
+              Software Engineer Fullstack <br className="sm:block hidden" />
+            </h3>
+          </div>
+          <small className="w-[100%] font-thin text-base text-text">
+            I love to build beautiful, intuitive and accessible digital
+            experiences with effective usability for the web.
+          </small>
+        </div>
 
-        {/* <img src={me} alt="me-hero-image" className="h-[80%] w-fill absolute right-64 bottom-24" /> */}
+        <img
+          src={me}
+          alt="me-hero-image"
+          className="h-[80%] w-[60%] absolute object-contain right-1"
+        />
       </div>
     </section>
   );
