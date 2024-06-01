6cbb9f2d983f6dd5d07e0cc4f819d9fe8a9d86a0
“florMignini” <flormignini29@gmail.com>
2024-06-01 08:55:46 -0300

    work experience section update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -55,19 +55,19 @@ export const Experience = () => {
       viewport={{ once: true, amount: 0.25 }}
       className="sm:px-16 px-6 sm:py-16 max-w-7xl mx-auto relative z-0"
     >
-      <span className="sm:p-10 lg:py-20" id="work">
+      <span className="sm:p-10 lg:py-24 text-text" id="work">
         &nbsp;
       </span>
       <motion.div variants={textVariant(0.1)}>
-        <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
+        <p className="sm:text-[18px] text-[14px] text-text uppercase tracking-wider">
           My Experience so far.
         </p>
-        <h2 className="text-black font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+        <h2 className="text-text-title font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
           Work Experience.
         </h2>
         <div className="w-full border-[1px] mb-3"/>
       </motion.div>
-      <div className="mt-20 flex flex-col">
+      <div className="mt-1 flex flex-col">
         <VerticalTimeline>
           {experiences.map((experience: IExperience, index: number) => (
             <ExperienceCard key={index} {...experience} />
