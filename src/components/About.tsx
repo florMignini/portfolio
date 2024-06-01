61b04762ab82f32dd02bdbe482a44724293c118a
“florMignini” <flormignini29@gmail.com>
2024-06-01 08:14:57 -0300

    about section update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,24 +4,27 @@ import { ReactNode } from "react";
 import { motion } from "framer-motion";
 import { fadeIn, staggerContainer, textVariant } from "../motion";
 import { ServiceCard } from ".";
-import { BackendIcon, FrontEndIcon, MobileIcon } from "../icons";
+import { BackendIcon, FrontEndIcon, /* MobileIcon */ } from "../icons";
 export type serviceType = {
   title: string;
   icon: ReactNode;
   index?: any;
+  content?: string;
 };
 const services: serviceType[] = [
   {
     title: "Frontend Developer",
     icon: <FrontEndIcon />,
+    content: "Creating user interface and user interactivity for the best experience."
   },
-  {
+  /* {
     title: "React Native Developer",
     icon: <MobileIcon />,
-  },
+  }, */
   {
     title: "Backend Developer",
     icon: <BackendIcon />,
+    content: "Building strong and robust full-stack structures for working workflow proccesses."
   },
 ];
 
@@ -34,33 +37,33 @@ export const About = () => {
       viewport={{ once: true, amount: 0.25 }}
       className="sm:px-16 px-6 sm:py-16 max-w-7xl mx-auto relative z-0"
     >
-      <span className="sm:p-14 py-36" id="about">
+      <span className="sm:p-14 py-24" id="about">
         &nbsp;
       </span>
      <div className="flex flex-col 2xl:pt-[13rem]">
 
-     <motion.div className="p-1" variants={textVariant(0.1)}>
+     <motion.div className="pt-10 text-text" variants={textVariant(0.1)}>
         <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
           About me.
         </p>
-        <h2 className="text-black font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+        <h2 className="text-text-title font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
           Overwiew.
         </h2>
 
-        <div className="w-full border-[1px] mb-3" />
+        <div className="w-full border-[1px] mb-1" />
       </motion.div>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="px-2 py-4 text-secondary text-base max-w-3xl leading-normal"
+        className="px-2 py-4 text-secondary text-base max-w-3xl leading-normal text-text"
       >
         Hey, I'm Florencia Mignini. I am a fullstack developer and UX/UI
-        designer. Passionate about music, I am always curious to learn more
+        designer. Passionate about music, waves lover. I am always curious to learn more
         about new technologies and creative coding.
       </motion.p>
      </div>
 
       {/* tech cards */}
-      <div className=" flex flex-wrap items-center justify-evenly">
+      <div className=" flex flex-wrap items-center justify-start gap-4 text-text-title">
         {services.map((service: serviceType, index: any) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,31 +1,30 @@
-import { Tilt } from "react-tilt"
-import {motion} from "framer-motion"
-import { serviceType } from "."
-import { fadeIn } from "../motion"
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { serviceType } from ".";
+import { fadeIn } from "../motion";
 
-export const ServiceCard = ({index, title, icon}:serviceType) => {
+export const ServiceCard = ({ index, title, icon, content }: serviceType) => {
   return (
-    <Tilt
-    className="xs:w-[250px] w-full py-3"
-    >
-        <motion.div
-        variants={fadeIn("right", "spring", 0.5*index, 0.75)}
+    <Tilt className="xs:w-[250px] w-full py-3 text-white">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          // options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-6 min-h-[220px] flex justify-start items-start flex-col gap-6"
         >
-<div 
-options={
-{max:45,
-scale:1,
-speed:450,
-}}
-className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
->
-  {icon}
-  <h3
-  className="text-white text-base font-bold text-center"
-  >{title}</h3>
-</div>
-        </motion.div>
+          {/* top card section */}
+         <div className="h-[40%] w-[100%] flex flex-col items-center justify-start text-text-title">
+         <div className="w-[100%]">{icon}</div>
+          <h3 className="w-[100%] text-lg font-bold text-start">{title}</h3>
+         </div>
+          {/* bottom card section */}
+          <div className="h-[50%] w-[100%] flex flex-col items-center justify-start">
+            <h6 className="text-sm font-light text-black">{content}</h6>
+          </div>
+        </div>
+      </motion.div>
     </Tilt>
-  )
-}
+  );
+};
