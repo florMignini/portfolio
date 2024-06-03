0734ac8ce20e91f3781928e77fc4c0a1eb1363b6
“florMignini” <flormignini29@gmail.com>
2024-06-03 07:34:37 -0300

    tech stack section update

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -82,25 +82,25 @@ export const TechStack = () => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: true, amount: 0.25 }}
-      className="sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0"
+      className="sm:px-16 px-6 max-w-7xl mx-auto relative z-0"
     >
-      <span className="sm:p-10 p-5" id="">
+      <span className="sm:p-10 lg:py-24 text-text" id="">
         &nbsp;
       </span>
       <motion.div variants={textVariant(0.1)}
       className="mb-10"
       >
-        <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
+        <p className="sm:text-[18px] text-[14px] text-text uppercase tracking-wider">
           Tech Stack.
         </p>
-        <h2 className="text-black font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+        <h2 className="text-text-title font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
         Tooling.
         </h2>
-        <div className="w-full border-[1px] mb-3"/>
+        <div className="w-full text-text border-[1px] mb-3"/>
       </motion.div>
       {/* content */}
       <div
-      className="flex flex-row flex-wrap justify-center gap-10 my-20"
+      className="flex flex-row flex-wrap justify-center gap-10 my-10"
       >
         {technologies.map((technology:ITech )=> (
           <div
